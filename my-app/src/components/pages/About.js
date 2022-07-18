643fb59f2587960e5a15550f223bba00c61ecefc
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-07-18 12:44:48 -0500

    aligned paragraphs for about me

--- my-app/src/components/pages/About.js
+++ my-app/src/components/pages/About.js
@@ -10,12 +10,12 @@ export default function About() {
         </div>
       </aside>
       <div id="aboutParagraphs" className='d-flex row col-8 justify-content-center'>
-        <p className="text-center">Hello! My name is Timothy Zalewski and the most exciting thing about me right now is that I made a huge career leap!
+        <p className="text-center col-12">Hello! My name is Timothy Zalewski and the most exciting thing about me right now is that I made a huge career leap!
             I was previously a pharmacist for 6 years, and have been in the pharmacy field for 9+ years. As a top performer, I 
             was one of the fastest and most accurate prescription verifiers in the highly desirable Austin Texas market. I chose to 
             transfer into Web Development as a way to be able to also apply my many other talents towards helping people.
         </p>
-        <p className="text-center">I am about half way through UT Austin's Web Development Coding Bootcamp and I am absolutely loving it! What drew me to this career 
+        <p className="text-center col-12">I am about half way through UT Austin's Web Development Coding Bootcamp and I am absolutely loving it! What drew me to this career 
             (and continues to draw me) is how JavaScript, HTML, CSS, JQuery, Bootstrap, Node, etc. are all just part one giant puzzle wanting to be solved.
             And you know what? I absolutely love doing puzzles! It feels so good to see a project come together, and then release something really cool 
             at the end. One of my more notable talents is being able to pick up any new skill, and apply it quickly on a whole new level of efficiency.
