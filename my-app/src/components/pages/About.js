02b73c2af30a64ff33ede4dc0eb27e23d13e2dcb
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-07-18 17:26:50 -0500

    transition for 'hello' set up' still need to workout fade effect

--- my-app/src/components/pages/About.js
+++ my-app/src/components/pages/About.js
@@ -12,29 +12,29 @@ export default function About() {
           </p>
         </div>
       </aside>
-      <div id="aboutParagraphs" className=' col-12 col-lg-8'>
-        <p className="col-12">Hello!!
+      <div id="aboutParagraphsSection" className=' col-12 col-lg-8'>
+        <p className="col-12" id="hello">Hello!!
         </p>
-        <p className="col-12">
+        <p className="col-12 aboutParagraphs">
           My name is Timothy Zalewski. I'm a Full-Stack Web Developer in addition to having my Doctorate of Pharmacy.
           "Wait what!? How did that come to be?" Previously I had been in the pharmacy field serving the community of Austin.
           I had the opportunity to help patient's and customers navigate the ambiguity of our health care market.
           I was also considered a top performer, as I had one of the fastest and most accurate prescription processing times in highly competitive Austin district.
         </p>
-        <p className="col-12">
+        <p className="col-12 aboutParagraphs">
           As time went on, I found myself needing more avenues for creativity and innovation.
           I began researching the different options and found the tech field has many opportunities that highly emphasis both of these aspects.
           This is when I began to seriously consider a career switch. I spent hours talking with friends and working with JavaScript. And you know what?
           I absolutely loved it! I looked into boot camps and decided <a href="https://techbootcamps.utexas.edu/coding/" className="sectionParagraphLinks" target="_blank" rel="noopener noreferrer">UT Austin's Full-Stack Web Development</a> course would be a great fit for me.
         </p>
-        <p className="col-12">
+        <p className="col-12 aboutParagraphs">
           Fastforward the next 12 weeks of intensive, Full-Stack, fast-paced web developement and here I am graduated as a Full-Stack Web Developer!
           I learned so much during that time frame, where I comitted about 60-80 hours a week coding.
           Along the way I met some great people and formed some new friendships along the way. 
           We focused heavily on JavaScript which includes: Express.js, Node.js, and React.js. which was great as it built a strong foundation of understanding coding.
           The boot camp also offered post course work, where I took it upon myself to learn Python and C#.
         </p>
-        <p className="col-12">
+        <p className="col-12 aboutParagraphs">
           What helps keep coding so interesting to me is how the entire process is like putting together a puzzle or playing a strategy game.
           I love doing puzzles, recognizing patterns, and applying unique yet crafted solutions towards a challenge.
           For more about me,
