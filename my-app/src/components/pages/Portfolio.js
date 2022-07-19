9fc8fffec3639aa0030b8dcc3d88c8a5901ac94e
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-07-19 13:16:16 -0500

    modularized project object

--- my-app/src/components/pages/Portfolio.js
+++ my-app/src/components/pages/Portfolio.js
@@ -1,53 +1,7 @@
 import React from 'react';
 import Project from './portfolio/Project'
 import '../../styles/Portfolio.css'
-
-// Object of all projects
-const projectObj = {
-  project1: {
-    title: 'Karmatic',  
-    github: 'https://github.com/Tim-Zebra/Karmatic',
-    deployed: 'https://serene-tor-70368.herokuapp.com/',
-    prgms: 'React, MongoDB, Apollo/GraphQL',
-    image: 'images/projectImages/karmatic.png',
-  },  
-  project2: {
-    title: 'Clinical Platform',  
-    github: 'https://github.com/Tim-Zebra/Clinical-Platform-Streamlined',
-    deployed: 'https://timzebratechblog.herokuapp.com/',
-    prgms: 'Express, SQL, Sequelize, handlebars',
-    image: 'images/projectImages/clinicalPlatformStreamLined.png',
-  },
-  project3: {
-    title: 'Love/Compatability Finder',  
-    github: 'https://github.com/Tim-Zebra/Compatible-or-Not_Find-true-love-here.',
-    deployed: 'https://tim-zebra.github.io/Compatible-or-Not_Find-true-love-here./',
-    prgms: 'APIs, HTML, CSS, JS, Collaboration',
-    image: 'images/projectImages/compatibility.gif',
-  },
-  project4: {
-    title: 'Team Builder',  
-    github: 'https://github.com/Tim-Zebra/Team-Builder',
-    deployed: false,
-    prgms: 'Node, Jest',
-    image: 'images/projectImages/teamBuilder.gif',
-},
-  project5: {
-    title: 'Badge Maker',  
-    github: 'https://github.com/Tim-Zebra/C-Sharp-CatWorx',
-    deployed: false,
-    prgms: 'C#',
-    image: 'images/projectImages/C-Sharp-Badge.png',
-  },
-  project6: {
-    title: 'News Feed',  
-    github: 'https://github.com/Tim-Zebra/python-newsfeed',
-    deployed: 'https://python-newsfeed-timzebra.herokuapp.com/',
-    prgms: 'Python, MongoDB',
-    image: 'images/projectImages/python-news-feed.png',
-  },
-
-}
+import { projectObj } from './portfolio/projectInfo';
 
 // Specified project values are passed to the Project component
 export default function Portfolio() {
---
+++ my-app/src/components/pages/portfolio/projectInfo.js
@@ -0,0 +1,46 @@
+// Object of all projects
+export const projectObj = {
+  project1: {
+    title: 'Karmatic',  
+    github: 'https://github.com/Tim-Zebra/Karmatic',
+    deployed: 'https://serene-tor-70368.herokuapp.com/',
+    prgms: 'React, MongoDB, Apollo/GraphQL',
+    image: 'images/projectImages/karmatic.png',
+    about: ''
+  },  
+  project2: {
+    title: 'Clinical Platform',  
+    github: 'https://github.com/Tim-Zebra/Clinical-Platform-Streamlined',
+    deployed: 'https://timzebratechblog.herokuapp.com/',
+    prgms: 'Express, SQL, Sequelize, handlebars',
+    image: 'images/projectImages/clinicalPlatformStreamLined.png',
+  },
+  project3: {
+    title: 'Love/Compatability Finder',  
+    github: 'https://github.com/Tim-Zebra/Compatible-or-Not_Find-true-love-here.',
+    deployed: 'https://tim-zebra.github.io/Compatible-or-Not_Find-true-love-here./',
+    prgms: 'APIs, HTML, CSS, JS, Collaboration',
+    image: 'images/projectImages/compatibility.gif',
+  },
+  project4: {
+    title: 'Team Builder',  
+    github: 'https://github.com/Tim-Zebra/Team-Builder',
+    deployed: false,
+    prgms: 'Node, Jest',
+    image: 'images/projectImages/teamBuilder.gif',
+},
+  project5: {
+    title: 'Badge Maker',  
+    github: 'https://github.com/Tim-Zebra/C-Sharp-CatWorx',
+    deployed: false,
+    prgms: 'C#',
+    image: 'images/projectImages/C-Sharp-Badge.png',
+  },
+  project6: {
+    title: 'News Feed',  
+    github: 'https://github.com/Tim-Zebra/python-newsfeed',
+    deployed: 'https://python-newsfeed-timzebra.herokuapp.com/',
+    prgms: 'Python, MongoDB',
+    image: 'images/projectImages/python-news-feed.png',
+  },
+}
