3c3e1a45017ebf888b73627e69fde8faadb44c6d
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-07-19 14:54:39 -0500

    commented out DEPLOYED AT, due to lack of reference at this moment. Able to select and modularized return statements

--- my-app/src/components/pages/portfolio/Project.js
+++ my-app/src/components/pages/portfolio/Project.js
@@ -24,29 +24,33 @@ export default function Project({projectNum, currentProject, selectedStatus, han
 
 // Mouse enter effects
   const [isSelected, setIsSelected] = useState(selectedStatus);
-  
-  return (
-    <div className='d-flex col-12 col-lg-5 col-xl-4 my-3 projectDiv'
-    onClick={() =>  handleSelection(true, currentProject.title, projectNum)}>
-      {!selectedStatus && firstLanding &&
-
+  if (!selectedStatus && firstLanding) {
+    return (
+      <div className='d-flex col-12 col-lg-5 col-xl-4 my-3 projectDiv'
+      onClick={() =>  handleSelection(true, currentProject.title, projectNum)}>
         <img src={currentProject.image} alt="project screen shot" className="projectBackground" id="backgroundImage" style={isFirstLandingStyle}></img>
-
-      }
-      {!selectedStatus && !firstLanding &&
+      </div>
+    );
+  } else if (!selectedStatus && !firstLanding) {
+    return (
+      <div className='d-flex col-12 col-lg-5 col-xl-4 my-3 projectDiv'
+      onClick={() =>  handleSelection(true, currentProject.title, projectNum)}>
         <img src={currentProject.image} alt="project screen shot" className="projectBackground" id="backgroundImage" style={isNotSelectedStyle}></img>
-      }
-      {selectedStatus && !firstLanding &&
-        <div class="projectContent d-flex">
+      </div>
+    );
+  } else if (selectedStatus && !firstLanding) {
+    return (
+      <div className='d-flex col-12 col-lg-5 col-xl-4 my-3 projectDiv'
+      onClick={() =>  handleSelection(true, currentProject.title, projectNum)}>
+        <div className="projectContent d-flex">
           <div className="projectProgramsUsed text-center">{currentProject.prgms}</div>
-          <a href={currentProject.deployed} target="_blank" rel="noreferrer" className="projectDeployedAt text-center">Deployed @</a>
+          {/* <a href={currentProject.deployed} target="_blank" rel="noreferrer" className="projectDeployedAt text-center">Deployed @</a> */}
           <div className="projectGithubLink">
             <IconCreator activeIconObj={iconLinkObj}/>
           </div>
           <img src={currentProject.image} alt="project screen shot" className="projectBackground" id="backgroundImage" style={isSelectedStyle}></img>
         </div>
-      }
-
-    </div>
-  );
+      </div>
+    );
+  }
 }
