7677fdaaffd3a05502116fca7dc5ff589e6d5e42
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-07-18 21:48:23 -0500

    working on CSS flex for portfolio. Work in progress

--- my-app/src/components/pages/portfolio/Project.js
+++ my-app/src/components/pages/portfolio/Project.js
@@ -13,22 +13,24 @@ const iconLinkObj= {
   const [isShown, setIsShown] = useState(false);
   
   return (
-    <div className='col-12 col-lg-6 col-xl-4 my-3 projectDiv'
+    <div className='d-flex col-12 col-lg-5 col-xl-4 my-3 projectDiv'
     onMouseEnter={() => setIsShown(true)}
     onMouseLeave={() => setIsShown(false)}>
       <img src={currentProject.image} alt="project screen shot" className="projectBackground">
       </img>
-      {isShown &&
-        <div className="projectProgramsUsed text-center">{currentProject.prgms}</div>
-      }
-      {isShown && currentProject.deployed !== false &&
-        <a href={currentProject.deployed} target="_blank" rel="noreferrer" className="projectDeployedAt text-center">Deployed @</a>
-      }
-      {isShown &&
-        <div className="projectGithubLink">
-          <IconCreator activeIconObj={iconLinkObj}/>
-        </div>
-      }
+      <div class="projectContent d-flex">
+        {isShown &&
+          <div className="projectProgramsUsed text-center">{currentProject.prgms}</div>
+        }
+        {isShown && currentProject.deployed !== false &&
+          <a href={currentProject.deployed} target="_blank" rel="noreferrer" className="projectDeployedAt text-center">Deployed @</a>
+        }
+        {isShown &&
+          <div className="projectGithubLink">
+            <IconCreator activeIconObj={iconLinkObj}/>
+          </div>
+        }
+      </div>
     </div>
   );
 }
