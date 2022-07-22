57f742fe48a6ff4a7a4fefde2464ac91edd36005
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-07-22 17:52:32 -0500

    rearranged nav tabs to prioritize resume

--- my-app/src/components/navigation/NavTabs.js
+++ my-app/src/components/navigation/NavTabs.js
@@ -24,20 +24,20 @@ function NavTabs({ currentPage, handlePageChange }) {
       </li>
       <li className="nav-item">
         <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
           href="#resume"
           onClick={() => handlePageChange('Resume')}
           className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
         >
           Resume
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
         </a>
       </li>
     </ul>
