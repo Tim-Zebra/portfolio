3507eb76e97a25c5b25e5ef17e4bf42ba518a848
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-07-26 16:34:38 -0500

    updated about paragraphs

--- my-app/src/components/pages/About.js
+++ my-app/src/components/pages/About.js
@@ -147,18 +147,19 @@ export default function About() {
           onMouseOver={() => {setParagraph1('red')}}
           onMouseLeave={() => {setParagraph1('black')}}
         >
-          My name is Timothy Zalewski. I'm a Full-Stack Web Developer in addition to having my Doctorate of Pharmacy.
-          "Wait what!? How did that come to be?" Previously I had been in the pharmacy field serving the community of Austin.
-          I had the opportunity to help patient's and customers navigate the ambiguity of our health care market.
-          I was also considered a top performer, as I had one of the fastest and most accurate prescription processing times in highly competitive Austin district.
+          My name is Timothy Zalewski. I am a Full-Stack Web Developer in addition to having my Doctorate of Pharmacy.
+          "Wait what!? How did that come to be?" My career originally began in pharmacy where
+          I had the opportunity to help patients and customers navigate the ambiguity of our health care market.
+          As I continued to grow and challenge myself others began referring to me as a subject matter expert and top performer. 
+          I slide into the role of pharmacy manager and got to lead a lot of cool stuff including leading the COVID-19 vaccine role out at my store.
         </p>
         <p className="col-12 aboutParagraphs"
           style={paragraph2Boarder}
           onMouseOver={() => {setParagraph2('deepskyblue')}}
           onMouseLeave={() => {setParagraph2('black')}}
         >
-          As time went on, I found myself needing more avenues for creativity and innovation.
-          I began researching the different options and found the tech field has many opportunities that highly emphasis both of these aspects.
+          As time went on, I found myself looking into more avenues for creativity and innovation.
+          I began researching different options and discovered tech has many opportunities that highly emphasis both of these aspects.
           This is when I began to seriously consider a career switch. I spent hours talking with friends and working with JavaScript. And you know what?
           I absolutely loved it! I looked into boot camps and decided <a href="https://techbootcamps.utexas.edu/coding/" className="sectionParagraphLinks" target="_blank" rel="noopener noreferrer">UT Austin's Full-Stack Web Development</a> course would be a great fit for me.
         </p>
@@ -167,21 +168,24 @@ export default function About() {
           onMouseOver={() => {setParagraph3('limegreen')}}
           onMouseLeave={() => {setParagraph3('black')}}
         >
-          Fastforward the next 12 weeks of intensive, Full-Stack, fast-paced web developement and here I am graduated as a Full-Stack Web Developer!
-          I learned so much during that time frame, where I comitted about 60-80 hours a week coding.
-          Along the way I met some great people and formed some new friendships along the way. 
-          We focused heavily on JavaScript which includes: Express.js, Node.js, and React.js. which was great as it built a strong foundation of understanding coding.
+          Fastforward through the next 12 weeks of intensive, Full-Stack, fast-paced web developement and here I am graduated as a Full-Stack Web Developer!
+          I learned so much during that cramped time frame, in which I comitted about 60-80 hours per week coding.
+          We focused heavily on JavaScript which includes: Express.js, Node.js, and React.js. 
           The boot camp also offered post course work, where I took it upon myself to learn Python and C#.
+          Along the way I met some great people, formed some new friendships, and enjoyed getting to network with other techies.
+          {'(Always down for a game of Age of Empires 2, Zombicide, or even a quick pick-up soccer game!)'}
+          I am excited to see how the futue of my career developes, and look forward to experiencing all the tech field has to offer.
         </p>
         <p className="col-12 aboutParagraphs"
           style={paragraph4Boarder}
           onMouseOver={() => {setParagraph4('gold')}}
           onMouseLeave={() => {setParagraph4('black')}}
         >
-          What helps keep coding so interesting to me is how the entire process is like putting together a puzzle or playing a strategy game.
-          I love doing puzzles, recognizing patterns, and applying unique yet crafted solutions towards a challenge.
+          What keeps coding so interesting for me is how the entire process is like putting together a puzzle or playing a strategy game.
+          I love doing puzzles, recognizing patterns, and applying unique yet crafted solutions towards a particular challenge.
+          It is like getting played to play games, and I love it!
           For more about me,
-          be sure to check out my <a href="https://www.linkedin.com/in/timzalewski/" className="sectionParagraphLinks" target="_blank" rel="noopener noreferrer">LinkedIn</a> and <a href="https://github.com/Tim-Zebra" className="sectionParagraphLinks" target="_blank" rel="noopener noreferrer">GitHub</a> profiles 
+          be sure to connect with me on <a href="https://www.linkedin.com/in/timzalewski/" className="sectionParagraphLinks" target="_blank" rel="noopener noreferrer">LinkedIn</a> and <a href="https://github.com/Tim-Zebra" className="sectionParagraphLinks" target="_blank" rel="noopener noreferrer">GitHub</a>
           to see where I started, who I am, and how far I have come in such a short amount of time.   
         </p>
       </div>
