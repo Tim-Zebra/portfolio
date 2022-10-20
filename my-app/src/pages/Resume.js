66a93cd224106996ab833237f7bbfba5fadc8895
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-10-20 17:51:10 -0500

    added pseudocode for business card

--- my-app/src/pages/Resume.js
+++ my-app/src/pages/Resume.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 import pdf from '../assets/resume.pdf';
+import bCard from '../assets/bCard.pdf';
+
 export default function Resume() {
   return (
     <div class="d-flex justify-content-center my-5 py-5">
       <div class="w-100" id="resumeDownloadDiv">Download my Resume <a href={pdf} id="resumeLink" download="T.Zalewski_Full-Stack_Resume">Here</a></div>
       <div class="w-100">OR</div>
       <div class="w-100" id="resumeDownloadDiv">View my Resume <a href={pdf} id="resumeLink">Here</a></div>
+      <div class="w-100">OR</div>
+      <div class="w-100" id="resumeDownloadDiv">View my online business card <a href={bCard} id="resumeLink">Here</a></div>
     </div>);
 };
