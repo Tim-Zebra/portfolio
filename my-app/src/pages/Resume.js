b470101c1162e86c86fface7942651e6721d551a
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-10-21 12:08:50 -0500

    reorganized resume display

--- my-app/src/pages/Resume.js
+++ my-app/src/pages/Resume.js
@@ -6,10 +6,10 @@ import bCard from '../assets/bCard.pdf';
 export default function Resume() {
   return (
     <div class="d-flex justify-content-center my-5 py-5">
-      <div class="w-100" id="resumeDownloadDiv">Download my Resume <a href={pdf} id="resumeLink" download="T.Zalewski_Full-Stack_Resume">Here</a></div>
+      <div class="w-100" id="resumeDownloadDiv">View my Resume <a href={pdf} id="resumeLinkView">Here</a></div>
       <div class="w-100">OR</div>
-      <div class="w-100" id="resumeDownloadDiv">View my Resume <a href={pdf} id="resumeLink">Here</a></div>
+      <div class="w-100" id="resumeDownloadDiv">Download my Resume <a href={pdf} id="resumeLinkDownload" download="T.Zalewski_Full-Stack_Resume">Here</a></div>
       <div class="w-100">OR</div>
-      <div class="w-100" id="resumeDownloadDiv">View my online business card <a href={bCard} id="resumeLink">Here</a></div>
+      <div class="w-100" id="resumeDownloadDiv">View my online business card <a href={bCard} id="bCardLinkView">Here</a></div>
     </div>);
 };
